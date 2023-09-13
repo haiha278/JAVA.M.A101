49661cf1ec24f6d7d304fc736bd731ac845769fe
haiha278 <haiha278172@gmail.com>
2023-09-13 16:31:15 +0700

    calculate interest and savingBalance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SavingAccount saver1 = new SavingAccount();
+        SavingAccount saver2 = new SavingAccount();
+        saver1.setSavingsBalance(2000);
+        saver2.setSavingsBalance(3000);
+
+        saver1.setAnnualInterestRate(0.04);
+        saver2.setAnnualInterestRate(0.04);
+        saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
+        System.out.println("New Balance for Saver1 =" + saver1.getSavingsBalance());
+        System.out.println("New Balance for Saver1 =" + saver2.getSavingsBalance());
+
+        saver1.setAnnualInterestRate(0.05);
+        saver2.setAnnualInterestRate(0.05);
+        saver1.calculateMonthlyInterest();
+        saver2.calculateMonthlyInterest();
+        System.out.println("New Balance for Saver1 =" + saver1.getSavingsBalance());
+        System.out.println("New Balance for Saver1 =" + saver2.getSavingsBalance());
     }
 }
---
+++ src/SavingAccount.java
@@ -0,0 +1,36 @@
+public class SavingAccount {
+    private double annualInterestRate;
+    private double savingsBalance;
+
+    public SavingAccount() {
+        annualInterestRate = 0.0;
+        savingsBalance = 0.0;
+    }
+
+    public SavingAccount(double annualInterestRate, double savingsBalance) {
+        this.annualInterestRate = annualInterestRate;
+        this.savingsBalance = savingsBalance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public void setSavingsBalance(double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+    }
+
+    public double calculateMonthlyInterest() {
+        double calculateMonthlyInterest = savingsBalance * annualInterestRate / 12;
+        savingsBalance = savingsBalance + calculateMonthlyInterest;
+        return calculateMonthlyInterest;
+    }
+}
